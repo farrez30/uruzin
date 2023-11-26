fd854a8c9ca2dc9ffed83c8e8e5e3fb10c466e1f
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-11-26 14:48:55 +0700

    responsiveness komitmen kami section

--- src/components/KomitmenKami/KomitmenKami.jsx
+++ src/components/KomitmenKami/KomitmenKami.jsx
@@ -46,43 +46,111 @@ const KomitmenKami = () => {
     },
   ];
   return (
-    <section id="tentangkami" className="max-w-[1170px] mb-[152px] pt-[150px]">
-      <article className="flex-col flex justify-center gap-[21px] max-w-[1109px] mb-5">
-        <h2 className="font-poppins font-light text-[12px] leading-normal text-green-uruzin tracking-[2px] rounded-[10px] border border-solid border-green-uruzin py-[5px] px-7 max-w-max">
+    <section id="tentangkami" className="max-w-[1170px] mb-[152px] lg:pt-[150px]">
+      <div class="lg:hidden flex flex-row gap-[14px] mb-[35px]">
+        <div className="flex flex-col gap-[10px] w-[50%]">
+          {validasiLayanan.slice(0, 2).map(({ id, title, desc }) => {
+            return (
+              <div className="flex flex-col px-[40px] py-[39px] text-center w-[156px] h-[167px] rounded-[10px] bg-white-pure drop-shadow-[0_25px_100px_#44589d14]">
+                <div className={id === 2 ? "" : "mb-5"}>
+                  <h1 className="font-dm font-medium text-[35px] leading-[55px] -tracking-[1.5px] text-green-uruzin">
+                    {title}
+                    {id === 2 && (
+                      <small className="font-dm font-medium text-[10px] leading-[55px] -tracking-[1.5px] text-green-uruzin">
+                        Hari*
+                      </small>
+                    )}
+                  </h1>
+                </div>
+                <div>
+                  <p className="font-poppins font-normal text-[10px] leading-[15px] -tracking-[0.2px] text-dark-blue-uruzin">
+                    {desc}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex flex-col gap-[10px] w-[50%]">
+          {validasiLayanan.slice(2).map(({ id, title, desc }) => {
+            return (
+              <div className="flex flex-col px-[10px] py-[39px] text-center w-[156px] h-[167px] rounded-[10px] bg-white-pure drop-shadow-[0_25px_100px_#44589d14]">
+                <div className={id === 4 ? "" : "mb-5"}>
+                  <h1 className="font-dm font-medium text-[35px] leading-[55px] -tracking-[1.5px] text-green-uruzin">
+                    {title}
+                    {id === 4 && (
+                      <small className="font-dm font-medium text-[10px] leading-[55px] -tracking-[1.5px] text-green-uruzin">
+                        Tahun
+                      </small>
+                    )}
+                  </h1>
+                </div>
+                <div>
+                  <p className="font-poppins font-normal text-[10px] leading-[15px] -tracking-[0.2px] text-dark-blue-uruzin">
+                    {desc}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <article className="flex-col flex justify-center gap-[10px] lg:gap-[21px] max-w-[1109px] mb-[35px] lg:mb-5">
+        <h2 className="hidden lg:block font-poppins font-light text-[12px] leading-normal text-green-uruzin tracking-[2px] rounded-[10px] border border-solid border-green-uruzin py-[5px] px-7 max-w-max">
           KOMITMEN KAMI
         </h2>
-        <h1 className="font-poppins font-medium text-[40px] leading-[50px] text-dark-blue-uruzin -tracking-[0.5px]">Percayakan Urusan Anda kepada Uruzin</h1>
-        <p className="font-poppins font-normal text-[16px] leading-[35px] text-gray-uruzin tracking-[0px]">
+        <h1 className="font-poppins font-medium text-[35px] lg:text-[40px] leading-[50px] text-dark-blue-uruzin -tracking-[0.5px]">
+          Percayakan Urusan Anda kepada Uruzin
+        </h1>
+        <p className="font-poppins font-normal text-[16px] leading-[25px] lg:leading-[35px] text-gray-uruzin tracking-[0px]">
           Kami bukan sekadar penyedia layanan, tapi mitra dalam kesuksesan bisnis Anda. Percayakan semua urusan izin
           pada Uruzin, karena keberhasilan Anda adalah komitmen kami.
         </p>
       </article>
       {/* //? Section Bawah  ///////////////////////////////////////////////////////////////////////////////////////////// */}
       <div className="flex gap-[115px]">
-        <ul className="flex-col flex gap-10 my-[31.5px] w-max">
+        <ul className="flex-col flex gap-10 lg:my-[31.5px] lg:w-[50%] border border-dashed">
           {fiturLayanan.map(({ id, image, title, desc }) => {
             return (
-              <li className="flex">
-                <img src={image} alt={title} className="bg-light-green-uruzin max-h-9 p-2 rounded-lg mr-4" />
-                <div className="flex-col flex w-max">
-                  <h1 className="font-dm font-normal text-[18px] leading-normal text-primary-content-uruzin tracking-[0px] mb-[10px]">{title}</h1>
-                  <p className="font-poppins font-normal text-[15px] leading-[30px] text-secondary-content-uruzin tracking-[0px] max-w-[352px]">{desc}</p>
+              <li className="flex flex-col lg:flex-row border">
+                <img
+                  src={image}
+                  alt={title}
+                  className="bg-light-green-uruzin max-w-[41px] lg:max-h-9 p-2 rounded-[4px] mr-4"
+                />
+                <div className="flex-col flex">
+                  <h1 className="font-dm font-normal text-[20px] lg:text-[18px] leading-normal text-primary-content-uruzin tracking-[0px] mb-[10px]">
+                    {title}
+                  </h1>
+                  <p className="hidden lg:block font-poppins font-normal text-[15px] leading-[30px] text-secondary-content-uruzin tracking-[0px] max-w-[352px]">
+                    {desc}
+                  </p>
                 </div>
               </li>
             );
           })}
         </ul>
 
-        <div class="flex gap-[45px] w-[50%]">
+        <div class="hidden lg:flex flex-row gap-[45px] w-[50%]">
           <div className="flex flex-col gap-4 mt-[67px]">
             {validasiLayanan.slice(0, 2).map(({ id, title, desc }) => {
               return (
                 <div className="flex flex-col px-[87px] py-[98px] mb-[45px] text-center w-[300px] rounded-[10px] bg-white-pure drop-shadow-[0_25px_100px_#44589d14]">
                   <div className={id === 2 ? "" : "mb-5"}>
-                    <h1 className="font-dm font-medium text-[72px] leading-[55px] -tracking-[1.5px] text-green-uruzin">{title}{id === 2 && <small className="font-dm font-medium text-[20px] leading-[55px] -tracking-[1.5px] text-green-uruzin">Hari*</small>}</h1>
+                    <h1 className="font-dm font-medium text-[72px] leading-[55px] -tracking-[1.5px] text-green-uruzin">
+                      {title}
+                      {id === 2 && (
+                        <small className="font-dm font-medium text-[20px] leading-[55px] -tracking-[1.5px] text-green-uruzin">
+                          Hari*
+                        </small>
+                      )}
+                    </h1>
                   </div>
                   <div>
-                    <p className="font-poppins font-normal text-[17px] leading-[25px] -tracking-[0.2px] text-dark-blue-uruzin">{desc}</p>
+                    <p className="font-poppins font-normal text-[17px] leading-[25px] -tracking-[0.2px] text-dark-blue-uruzin">
+                      {desc}
+                    </p>
                   </div>
                 </div>
               );
@@ -93,10 +161,19 @@ const KomitmenKami = () => {
               return (
                 <div className="flex flex-col px-[70px] py-[98px] mb-[45px] text-center w-[300px] rounded-[10px] bg-white-pure drop-shadow-[0_25px_100px_#44589d14]">
                   <div className={id === 4 ? "" : "mb-5"}>
-                    <h1 className="font-dm font-medium text-[72px] leading-[55px] -tracking-[1.5px] text-green-uruzin">{title}{id === 4 && <small className="font-dm font-medium text-[20px] leading-[55px] -tracking-[1.5px] text-green-uruzin">Tahun</small>}</h1>
+                    <h1 className="font-dm font-medium text-[72px] leading-[55px] -tracking-[1.5px] text-green-uruzin">
+                      {title}
+                      {id === 4 && (
+                        <small className="font-dm font-medium text-[20px] leading-[55px] -tracking-[1.5px] text-green-uruzin">
+                          Tahun
+                        </small>
+                      )}
+                    </h1>
                   </div>
                   <div>
-                    <p className="font-poppins font-normal text-[17px] leading-[25px] -tracking-[0.2px] text-dark-blue-uruzin">{desc}</p>
+                    <p className="font-poppins font-normal text-[17px] leading-[25px] -tracking-[0.2px] text-dark-blue-uruzin">
+                      {desc}
+                    </p>
                   </div>
                 </div>
               );
